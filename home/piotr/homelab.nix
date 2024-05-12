18183d1ca01de9177c21a872760b9060d35dad03
Piotr Kwiecinski <piokwiecinski@gmail.com>
2024-05-12 20:32:25 +0200

    add slack, element, signal, intellij, gimp, inkscapem yt-dlp, openvpn

--- home/piotr/homelab.nix
+++ home/piotr/homelab.nix
@@ -21,12 +21,20 @@
   home.packages = with pkgs; [
     nodejs_20
     htop
-    signal-desktop
     fira-code
     fira-code-symbols
     fira-code-nerdfont
     jq
+    unstable.devenv
+    unstable.element-desktop
+    unstable.signal-desktop
+    unstable.slack
     unstable.spotify
+    unstable.jetbrains.idea-ultimate
+    inkscape
+    gimp
+    yt-dlp
+    openvpn
   ];
 
   programs.bash = {
