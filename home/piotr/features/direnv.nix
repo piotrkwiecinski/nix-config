b4c4402a9f835047726299fcc9cdf00367c1840a
Piotr Kwiecinski <piotr.kwiecinski@codemanufacture.com>
2024-05-23 10:52:23 +0200

    direnv: create cache in xdg cache instead of project dir

--- home/piotr/features/direnv.nix
+++ home/piotr/features/direnv.nix
@@ -4,5 +4,18 @@
     enable = true;
     enableBashIntegration = true;
     nix-direnv.enable = true;
+
+    stdlib = ''
+      : ''${XDG_CACHE_HOME:=$HOME/.cache}
+      declare -A direnv_layout_dirs
+
+      # https://github.com/direnv/direnv/wiki/Customizing-cache-location#hashed-directories
+      direnv_layout_dir() {
+        echo "''${direnv_layout_dirs[$PWD]:=$(
+          echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+          echo -n "$PWD" | sha1sum | cut -d ' ' -f 1
+        )}"
+      }
+    '';
   };
 }
