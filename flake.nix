a162ea98978c5e1f7701464c671ce9e6a67e54b5
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2024-12-13 16:15:15 +0100

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
     hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     emacs-overlay = {
