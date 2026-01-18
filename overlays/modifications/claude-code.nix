51de4c152eaf8950aabbb75868a55362bc179c72
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2026-01-18 17:52:41 +0100

    chore: update claude-code to 2.1.12

--- overlays/modifications/claude-code.nix
+++ overlays/modifications/claude-code.nix
@@ -2,12 +2,12 @@
 _final: prev: {
   unstable = prev.unstable // {
     claude-code = prev.unstable.claude-code.overrideAttrs (_: rec {
-      version = "2.1.2";
+      version = "2.1.12";
       src = prev.unstable.fetchzip {
         url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-        hash = "sha256-PpNXyZ3xoZ/4lCvtErltkdsL/1hDRyiicblvhykgROw=";
+        hash = "sha256-JX72YEM2fXY7qKVkuk+UFeef0OhBffljpFBjIECHMXw=";
       };
-      npmDepsHash = "sha256-KdVaAYXCy+oMN9b1lLeIRiGp/Zb29T4b3pvDp8O1v/M=";
+      npmDepsHash = "sha256-FxyNCFlsgjXAGCGqro+VRwkarif9SzqmrMz0xgmvBco=";
     });
   };
 }
