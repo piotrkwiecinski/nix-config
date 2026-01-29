425f131d0847c082faff2b8e4b8afca315c178f0
Piotr Kwiecinski <2151333+piotrkwiecinski@users.noreply.github.com>
2026-01-29 14:38:36 +0100

    hosts(thinkpad-x1-g3): limit boot configurations to 3

--- hosts/thinkpad-x1-g3/default.nix
+++ hosts/thinkpad-x1-g3/default.nix
@@ -47,6 +47,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
 
   services.fwupd.enable = true;
